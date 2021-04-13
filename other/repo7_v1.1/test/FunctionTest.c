38731f912127f03cfbe26210b0a21e723812d26d
Zhou Zhiyu <zhouzhiyu@tp-link.com.cn>
2021-04-13 15:12:59 +0800

    asdf

--- other/repo7_v1.1/test/FunctionTest.c
+++
@@ -1,9 +0,0 @@
-#include "../include/DMailListener.h"
-
-int main()
-{
-    DMailListener *pl = dmailListener_init(20, 20);
-    dmailListener_run(pl, NULL, MODE_ONLINE);
-    dmailListener_delete(pl);
-    return 0;
-}
